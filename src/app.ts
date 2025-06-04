20de9325fc05849b4f9fdcd0f349f6b7a96f2191
Vinicius Alves <vinialves.dev@gmail.com>
2025-06-04 18:54:23 -0300

    feat: create user session

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import session from '@fastify/session'
 import { usersRoutes } from './routes/usersRoutes'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(cookie)
+app.register(session, {
+  secret: env.SECRET_KEY_SESSION,
+  cookie: { secure: false, httpOnly: true },
+  saveUninitialized: false,
+})
+
 app.register(usersRoutes, {
   prefix: 'users',
 })
--- src/env/index.ts
+++ src/env/index.ts
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  SECRET_KEY_SESSION: z.string(),
   DATABASE_CLIENT: z.enum(['sqlite3', 'pg']),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
